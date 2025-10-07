4599adc9de1e6baa0a16fd60319eb00450a69391
ilay menahem <110230889+IlayMenahem@users.noreply.github.com>
2025-10-07 11:38:10 +0300

    Refactor plot_learning_process to accept variable lists

--- implementations/utils.py
+++ implementations/utils.py
@@ -2,39 +2,21 @@ import matplotlib.pyplot as plt
 import gymnasium as gym
 from torch import nn
 
-def plot_learning_process(scores: list[float], vals1: list[float], vals2: list[float]) -> None:
+def plot_learning_process(*lists):
     '''
-    Plots the training scores, losses, and epsilon values.
+    Plots the training process values.
 
     Args:
-    scores (List[float]): The training scores.
-    losses (List[float]): The training losses.
-    epsilons (List[float]): The epsilon values.
+    - *lists: A variable number of lists containing values to plot.
 
-    Returns:
-    None
     '''
+    plt.figure(figsize=(20, 10))
 
-    plt.figure(figsize=(20, 5))
-    plt.subplot(131)
-    window = 50
-    plt.plot(scores)
-    if len(scores) >= window:
-        running_avg = [sum(scores[max(0, i - window):i]) / window for i in range(len(scores))]
-        plt.plot(running_avg, label=f'Running Avg (window={window})')
-    plt.xlabel("Episode")
-    plt.ylabel("Score")
+    for i, vals in enumerate(lists):
+        plt.plot(vals, label=f'Series {i+1}')
+
+    plt.legend()
     plt.grid(True)
-    plt.subplot(132)
-    plt.title('loss')
-    plt.plot(vals1)
-    plt.xlabel("Update step")
-    plt.grid(True)
-    plt.subplot(133)
-    plt.plot(vals2)
-    plt.xlabel("Update step")
-    plt.grid(True)
-    plt.tight_layout()
     plt.show()
 
 
